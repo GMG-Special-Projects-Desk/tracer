5e3d9e3ce1c5c08593a845177ee0c0ca1efc7abc
Surya Mattu <surya@suryamattu.com>
2017-02-23 11:17:32 -0500

    tweaks

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ from settings import *
 import logging
 import sys
 
-def PrintException(logger):
+def PrintException(logging):
     exc_type, exc_obj, tb = sys.exc_info()
     f = tb.tb_frame
     lineno = tb.tb_lineno
     filename = f.f_code.co_filename
     linecache.checkcache(filename)
     line = linecache.getline(filename, lineno, f.f_globals)
-    logger.error('EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj))
+    logging.error('EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj))
 
 
 def parse_args():
@@ -97,6 +97,6 @@ if __name__ == '__main__':
 
     except Exception as e:
         exc_type, exc_obj, tb = sys.exc_info()
-        logger.error(e)
-        PrintException(logger)
+        logging.error(e)
+        PrintException(logging)
 
